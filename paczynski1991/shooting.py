632c57ec358b09e979daf93016e5485374abc152
NicholasRui <nrui@caltech.edu>
2025-05-30 10:46:27 -0400

    Removed x_match and just integrate from left to right

--- paczynski1991/shooting.py
+++ paczynski1991/shooting.py
@@ -12,14 +12,14 @@ import pdb
 # Rp = accretor polar radius = 1 (this defines Rp = R_star in P+91)
 
 # Dimensionless free parameters (Paczynski+1991)
-a_inv = 0.1
-ω_s = 1.#1.001
+log10_a = 6
+# ω_s = 0.1111 #
+ω_s = 0.7
 
 n = 1.5  # polytropic index (n=3/2 is γ=5/3)
 
 # Chosen for numerical reasons (x -> non-dim'd r)
-x_out = 10  # outer boundary condition (where disk is approximately Keplerian)
-x_match = 5  # matching point
+x_out = 3  # outer boundary condition (where disk is approximately Keplerian)
 x_in = 0.8  # inner boundary condition (within star but not totally)
 method = "Radau"
 
@@ -27,17 +27,15 @@ method = "Radau"
 # ω = dimensionless disk rotation / orbital rate
 # y = (thin) disk scale height
 
-
 # Define a function giving dZ / dx:
 def get_dZ_dx(x, Z, j_star):
     ω, y = Z
-    dω_dx = x ** (3. * n - 1.5) * (j_star - ω * x ** 2) * a_inv / y ** (2. * n + 3.)
-    # dω_dx = x ** 3 * (j_star - ω * x ** 2) * a_inv / y ** 6
+    # dω_dx = x ** (3. * n - 1.5) * (j_star - ω * x ** 2) * 10 ** -log10_a / y ** (2. * n + 3.)
+    dω_dx = x ** 3 * (j_star - ω * x ** 2) * 10 ** -log10_a / y ** 6 # n=3/2 specifically
     dy_dx = ω ** 2 * (x ** 2 + y ** 2) ** 1.5 * x / y - x / y
     dZ_dx = np.array([dω_dx, dy_dx])
 
     return dZ_dx
-
 
 # Define a function giving boundary condition at inner Z
 def get_Z_in():
@@ -48,77 +46,64 @@ def get_Z_in():
     return Z_in
 
 
-def get_Z_out(j_star):
-    # Fix ω and dω/dx to be Keplerian
-    ω_out = x_out ** -1.5
-    y_out = (2. * a_inv / 3.) ** (1. / (2. * n + 3.)) * x_out ** ((6. * n + 3.) / (4. * n + 6.)) * (1 - j_star * x_out ** -0.5) ** (1. / (2. * n + 3.))
-    # y_out = (2. * a_inv / 3.) ** (1. / 6.) * x_out * (1 - j_star * x_out ** -0.5) ** (1. / 6.)
-    Z_out = np.array([ω_out, y_out])
-
-    return Z_out
 
 
-def get_Z_match_from_in(j_star):
+
+# def get_Z_out(j_star):
+#     # Fix ω and dω/dx to be Keplerian
+#     ω_out = x_out ** -1.5
+#     y_out = (2. * 10 ** -log10_a / 3.) ** (1. / (2. * n + 3.)) * x_out ** ((6. * n + 3.) / (4. * n + 6.)) * (1 - j_star * x_out ** -0.5) ** (1. / (2. * n + 3.))
+#     # y_out = (2. * 10 ** -log10_a / 3.) ** (1. / 6.) * x_out * (1 - j_star * x_out ** -0.5) ** (1. / 6.)
+#     Z_out = np.array([ω_out, y_out])
+
+#     return Z_out
+
+
+def get_Z_out_from_in(j_star, atol=1e-10):
     """
     Integrate from inner boundary condition to get Z_match
 
     TODO: seems to break for j_star near one as dZ/dx diverges... is that physical?
     """
-    x_arr = np.array([x_in, x_match])
+    x_arr = np.array([x_in, x_out])
     res = solve_ivp(
         get_dZ_dx,
-        [x_in, x_match],
+        [x_in, x_out],
         get_Z_in(),
-        t_eval=[x_match],
+        t_eval=[x_out],
         args=[j_star],
         method=method,
+        atol=atol
     )
 
     assert res.success
     Z_match = res.y[:, 0]
 
     # test
-    x_arr = np.linspace(x_in, x_match, 100)
+    x_arr = np.linspace(x_in, x_out, 10000)
     res = solve_ivp(
         get_dZ_dx,
-        [x_in, x_match],
+        [x_in, x_out],
         get_Z_in(),
         t_eval=x_arr,
         args=[j_star],
         method=method,
+        atol=atol
     )
 
     x, Z = res.t, res.y
     ω, y = res.y[0], res.y[1]
 
+    x_a = np.linspace(0, 1, 100)
+    y_a = ((1 - 0.5 * ω_s**2 * x_a**2) ** -2.0 - x_a**2) ** 0.5
+
     plt.close()
     plt.plot(x, ω, label="ω")
     plt.plot(x, y, label="y")
+    plt.plot(x_a, y_a)
     plt.yscale("log")
     plt.legend(loc="upper right")
     plt.show()
-
-    return Z_match
-
-
-def get_Z_match_from_out(j_star):
-    """
-    Integrate from outer boundary condition to get Z_match
-
-    TODO: this just breaks entirely lmao
-    """
-    x_arr = np.array([x_out, x_match])
-    res = solve_ivp(
-        get_dZ_dx,
-        [x_out, x_match],
-        get_Z_out(j_star),
-        t_eval=[x_match],
-        args=[j_star],
-        method=method,
-    )
-
-    assert res.success
-    Z_match = res.y[:, 0]
 
     return Z_match
 
