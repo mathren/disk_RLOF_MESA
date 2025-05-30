9b046a8a6ecfe4b3f803c808cc78f27d56dc49b5
Mathieu Renzo <mrenzo@arizona.edu>
2025-05-30 10:31:56 -0700

    rm print statements

--- MESA_Setup/src/run_binary_extras.f90
+++ MESA_Setup/src/run_binary_extras.f90
@@ -147,8 +147,6 @@
             return
          end if
 
-         print *, "accretion mode", b% accretion_mode
-         print *, "use other j", b% use_other_accreted_material_j
       end function  extras_binary_start_step
 
       !Return either keep_going, retry or terminate
